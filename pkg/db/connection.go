93c9e76c4240bfb116735d5f3d3cbc4147669d36
Nikhilnarayanan623 <Nikhilnarayan623@gmail.com>
2023-03-06 12:02:16 +0530

    database connection and migration of tables completed and checked foreign key and primary key relation

--- pkg/db/connection.go
+++ pkg/db/connection.go
@@ -1,1 +1,43 @@
 package db
+
+import (
+	"fmt"
+
+	"github.com/nikhilnarayanan623/ecommerce-gin-clean-arch/pkg/config"
+	"github.com/nikhilnarayanan623/ecommerce-gin-clean-arch/pkg/domain"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// func to connect data base using config(database config) and return address of a new instnce of gorm DB
+func ConnectDatbase(cfg config.Config) (*gorm.DB, error) {
+
+	dsn := fmt.Sprintf("host=%s user=%s dbname=%s port=%s password=%s", cfg.DBHost, cfg.DBUser, cfg.DBName, cfg.DBPort, cfg.DBPassword)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		SkipDefaultTransaction: true,
+	})
+
+	// migrate the database tables
+	db.AutoMigrate(
+		//user
+		domain.Users{},
+		domain.Country{},
+		domain.Address{},
+		domain.UserAdress{},
+
+		//admin
+		domain.Admin{},
+
+		//product
+		domain.ProductCategory{},
+		domain.Product{},
+		domain.Variation{},
+		domain.VariationOption{},
+		domain.ProductItem{},
+		domain.ProductConfiguraion{},
+		domain.ProductImage{},
+	)
+
+	return db, err
+}
