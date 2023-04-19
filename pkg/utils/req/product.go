3785a241ade3f1004afffe8966437797afda0a8e
nikhilnarayanan623 <nikhilnarayanan623@gmail.com>
2023-04-19 13:08:14 +0530

    patches on add product and update product

--- pkg/utils/req/product.go
+++ pkg/utils/req/product.go
@@ -2,6 +2,13 @@ package req
 
 // for a new product
 type ReqProduct struct {
+	ProductName string `json:"product_name" gorm:"not null" binding:"required,min=3,max=50"`
+	Description string `json:"description" gorm:"not null" binding:"required,min=10,max=100"`
+	CategoryID  uint   `json:"category_id" binding:"required"`
+	Price       uint   `json:"price" gorm:"not null" binding:"required,numeric"`
+	Image       string `json:"image" gorm:"not null" binding:"required"`
+}
+type ReqProductUpdate struct {
 	ID          uint   `json:"id" binding:"required"`
 	ProductName string `json:"product_name" gorm:"not null" binding:"required,min=3,max=50"`
 	Description string `json:"description" gorm:"not null" binding:"required,min=10,max=100"`
